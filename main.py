5d082d9084d5859fba3d6cf1c110560609445471
chrzzl <christoph.karg@mdc-berlin.de>
2024-12-06 12:19:48 +0100

    add greedy and nearest_within_radius matching

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def main():
     # 1) Load graphs with attributes
     # NOTE: We currently expect all graphs to be Branchings, see https://networkx.org/documentation/stable/reference/algorithms/tree.html
     G, H = two_trees()
-    assert nx.is_branching(G) and nx.is_branching(H), "Graphs must be branching graphs"
+    assert nx.is_branching(G) and nx.is_branching(H), "Graphs must be branchings"
 
     # 2) Match graphs
     # Match target graph (e.g. prediction) to source graph (e.g. ground truth)
     # The matching variable is a dictionary that maps nodes in target graph to nodes in source graph.
     # NOTE: This is not symmetric, match(G,H) in general does not equal match(H,G).
-    matching = match_graphs(source=G, target=H, matching_type=MatchingType.Nearest)
+    matching = match_graphs(source=G, target=H, matching_type=MatchingType.Greedy)
 
     # 3) Compute metrics wrt to source and matched target graph
     results_dict = evaluate_all_metrics(G, H, matching)
