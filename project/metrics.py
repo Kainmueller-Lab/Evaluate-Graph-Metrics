fix normalization in smd

project/metrics.py
@@ -58,7 +58,7 @@ class GraphMetrics:
 
 
     @staticmethod
-    def street_mover_distance(G, H):
+    def street_mover_distance(G, H, normalize=True):
         '''
         Taken from https://github.com/davide-belli/generative-graph-transformer/blob/master/metrics/streetmover_distance.py
         '''
@@ -74,28 +74,27 @@ class GraphMetrics:
         G_nodes = [torch.from_numpy(np.array(arr, dtype=np.float32)) for arr in G_coords.values()]
         H_nodes = [torch.from_numpy(np.array(arr, dtype=np.float32)) for arr in H_coords.values()]
 
-        # NOTE: For debuggig
-        # if normalize:
-        #     G_nodes_tensor = torch.stack(G_nodes)
-        #     H_nodes_tensor = torch.stack(H_nodes)
+        if normalize: # Normalize coordinates such that they are between 0 and 1
+            G_nodes_tensor = torch.stack(G_nodes)
+            H_nodes_tensor = torch.stack(H_nodes)
 
-        #     def min_max(tensor_A, tensor_B):
-        #         # Concatenate both tensors to include all points
-        #         all_points = torch.cat([tensor_A, tensor_B], dim=0)
+            def min_max(tensor_A, tensor_B):
+                # Concatenate both tensors to include all points
+                all_points = torch.cat([tensor_A, tensor_B], dim=0)
 
-        #         # Compute the bounding box
-        #         min = all_points.min(dim=0).values  # 1x3 - min x, y, z
-        #         max = all_points.max(dim=0).values  # 1x3 - max x, y, z
-        #         return min, max
+                # Compute the bounding box
+                min = all_points.min(dim=0).values  # 1x3 - min x, y, z
+                max = all_points.max(dim=0).values  # 1x3 - max x, y, z
+                return min, max
             
-        #     def normalize(tensor, min, max):
-        #         return (tensor - min) / (max - min)
-        #     min, max = min_max(G_nodes_tensor, H_nodes_tensor)
+            def normalize(tensor, min, max):
+                return (tensor - min) / (max - min)
+            min, max = min_max(G_nodes_tensor, H_nodes_tensor)
 
-        #     G_nodes_normalized = normalize(G_nodes_tensor, min, max)
-        #     H_nodes_normalized = normalize(H_nodes_tensor, min, max)
-        #     G_nodes = [coord for coord in G_nodes_normalized]
-        #     H_nodes = [coord for coord in H_nodes_normalized]
+            G_nodes_normalized = normalize(G_nodes_tensor, min, max)
+            H_nodes_normalized = normalize(H_nodes_tensor, min, max)
+            G_nodes = [coord for coord in G_nodes_normalized]
+            H_nodes = [coord for coord in H_nodes_normalized]
 
             # print(f"Normalized G_nodes range: {G_nodes_normalized.min(dim=0).values}, {G_nodes_normalized.max(dim=0).values}")
             # print(f"Normalized H_nodes range: {H_nodes_normalized.min(dim=0).values}, {H_nodes_normalized.max(dim=0).values}")