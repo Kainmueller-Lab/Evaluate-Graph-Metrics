25e00d8c108c5fae016b5d1d26c7ba312f56fd26
Rajalakshmi <rajalakshmip.95@gmail.com>
2025-07-15 17:44:55 +0200

    checkout push

--- batch_evaluate.py
+++ batch_evaluate.py
@@ -4,8 +4,8 @@ import re
 from collections import defaultdict
 
 
-GT_DIR = "/mnt/md0/rajalakshmi/Unet_seg/real_data/catmaid_annotations/1732_GT/"
-PRED_DIR = "/mnt/md0/rajalakshmi/Unet_seg/real_data/skeletons/teasar_swc/1732/"
+GT_DIR = "/mnt/md0/rajalakshmi/Unet_seg/whole_pipeline_with_zarr_funlib/ground_truth_graph/"
+PRED_DIR = "/mnt/md0/rajalakshmi/Unet_seg/experiments/rc_50i_1000c_20c_scale/inference_nonlinear_150epoch/teasar_swc/"
 #GT_DIR = "/mnt/md0/rajalakshmi/Unet_seg/whole_pipeline_with_zarr_funlib/ground_truth_graph/"
 # PRED_DIR = "/mnt/md0/rajalakshmi/Unet_seg/whole_pipeline_with_zarr_funlib/vesselformer/teasar_swc/"
 MAIN_SCRIPT = "main.py"
